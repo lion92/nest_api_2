63fcc32e6fcd67668735336f332eb044412c101d
kriss <kriss.clotilde@gmail.com>
2023-05-12 19:27:45 +0200

    cors correction

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,26 +1,29 @@
-import {Module} from '@nestjs/common';
-import {AppController} from './app.controller';
-import {AppService} from './app.service';
-import {TypeOrmModule} from "@nestjs/typeorm";
-import {TodosModule} from "./todos/todos.module";
-import {ConnectionModule} from './connection/connection.module';
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TodosModule } from './todos/todos.module';
+import { ConnectionModule } from './connection/connection.module';
 
 @Module({
-    imports: [
-        TypeOrmModule.forRoot({
-            type: 'mysql',
-            host: 'localhost',
-            port: 3306,
-            username: 'root',
-            password: 'Yxrhyrup1990!!',
-            database: 'crud_nest',
-            entities: ["dist/**/*.entity{.ts,.js}"],
-            synchronize: true,
-        }), TodosModule, ConnectionModule],
-    controllers: [AppController],
-    providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'Yxrhyrup1990!!',
+      database: 'crud_nest',
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    TodosModule,
+    ConnectionModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {
-    constructor() {
-    }
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  constructor() {}
 }
--- src/connection/connection.service.ts
+++ src/connection/connection.service.ts
@@ -1,48 +1,49 @@
-import {Injectable, NotFoundException, UnauthorizedException} from '@nestjs/common';
-import {UserDTO} from "../dto/UserDTO";
-import {InjectRepository} from "@nestjs/typeorm";
-import {Repository} from "typeorm";
-import {User} from "../entity/User.entity";
+import {
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { UserDTO } from '../dto/UserDTO';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from '../entity/User.entity';
 import * as bcrypt from 'bcrypt';
-import {LoginDTO} from "../dto/LoginDTO";
+import { LoginDTO } from '../dto/LoginDTO';
 
 @Injectable()
 export class ConnectionService {
-    constructor(
-        @InjectRepository(User)
-        private userRepository: Repository<User>
-    ) {
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {}
+
+  async signup(user: UserDTO) {
+    const userCreate = user;
+    const saltOrRounds = 10;
+    const password = userCreate.password;
+    userCreate.password = await bcrypt.hash(password, saltOrRounds);
+    await this.userRepository.save(userCreate);
+  }
+
+  async login(
+    user: LoginDTO,
+  ): Promise<{ id: number; email: string; prenom: string; nom: string }> {
+    const { password, email } = user;
+    const userFind = await this.userRepository.findOneBy({ email: email });
+    if (!userFind) {
+      throw new NotFoundException('User Not found');
+    } else {
+      const match = await bcrypt.compare(password, userFind.password);
+      if (!match) {
+        throw new UnauthorizedException('illegal');
+      } else {
+        return {
+          id: userFind.id,
+          email: userFind.email,
+          nom: userFind.nom,
+          prenom: userFind.prenom,
+        };
+      }
     }
-
-
-    async signup(user: UserDTO) {
-        const userCreate = user;
-        const saltOrRounds = 10;
-        const password = userCreate.password;
-        const hash = await bcrypt.hash(password, saltOrRounds);
-        userCreate.password = hash
-        await this.userRepository.save(userCreate);
-    }
-
-    async login(user: LoginDTO): Promise<{ id: number, email: string, prenom: string, nom: string }> {
-        const {password, email} = user;
-        const userFind = await this.userRepository.findOneBy({email: email})
-        if (!userFind) {
-            throw new NotFoundException('User Not found')
-        } else {
-            const match = await bcrypt.compare(password, userFind.password)
-            if (!match) {
-                throw new UnauthorizedException('illegal')
-            } else {
-                return {
-                    "id": userFind.id,
-                    "email": userFind.email,
-                    "nom": userFind.nom,
-                    "prenom": userFind.prenom
-                };
-            }
-        }
-
-
-    }
+  }
 }
