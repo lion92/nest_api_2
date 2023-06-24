e9b89574967882f3d63fee6fff0e0997c391302c
kriss <kriss.clotilde@gmail.com>
2023-06-24 13:08:38 +0200

    pour linux

--- src/connection/connection.controller.spec.ts
+++ src/connection/connection.controller.spec.ts
@@ -1,18 +1,18 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { ConnectionController } from './connection.controller';
+import {Test, TestingModule} from '@nestjs/testing';
+import {ConnectionController} from './connection.controller';
 
 describe('ConnectionController', () => {
-  let controller: ConnectionController;
+    let controller: ConnectionController;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [ConnectionController],
-    }).compile();
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            controllers: [ConnectionController],
+        }).compile();
 
-    controller = module.get<ConnectionController>(ConnectionController);
-  });
+        controller = module.get<ConnectionController>(ConnectionController);
+    });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
+    it('should be defined', () => {
+        expect(controller).toBeDefined();
+    });
 });
--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule,{
+    logger: console
+  });
   app.enableCors({
     allowedHeaders: '*',
     origin: '*',
   });
-  await app.listen(3000);
+  await console.log("ok")
+  await app.listen(3004);
 }
 bootstrap();
