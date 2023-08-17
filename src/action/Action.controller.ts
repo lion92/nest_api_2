9f4716e3d3fb2ec1f4cb5c5773b5b6dd76ca6d97
kriss <kriss.clotilde@gmail.com>
2023-08-17 02:59:47 +0200

    description

--- src/action/Action.controller.ts
+++ src/action/Action.controller.ts
@@ -1,7 +1,7 @@
-import {Body, Controller, Delete, Get, Param, Post, Put} from '@nestjs/common';
+import {Body, Controller, Delete, Get, Param, Post, Put, Res} from '@nestjs/common';
 import {ActionService} from "./Action.service";
 import {ActionDTO} from "../dto/ActionDTO";
-import {Action} from "../entity/Action.entity";
+import {map} from "rxjs";
 
 @Controller('action')
 export class ActionController {
@@ -18,6 +18,102 @@ export class ActionController {
         return await this.actionService.findCategorieSum().then(value => value);
     }
 
+    @Get("/export")
+    async export(@Res() res) {
+        const excel = require('node-excel-export');
+        let listMontants = await this.actionService.findAll();
+        console.log(listMontants.map(value => { return { montant:value.montant}}));
+// You can define styles as json object
+        const styles = {
+            headerDark: {
+                fill: {
+                    fgColor: {
+                        rgb: 'FF000000'
+                    }
+                },
+                font: {
+                    color: {
+                        rgb: 'FFFFFFFF'
+                    },
+                    sz: 14,
+                    bold: true,
+                    underline: true
+                }
+            },
+            cellPink: {
+                fill: {
+                    fgColor: {
+                        rgb: 'FFFFCCFF'
+                    }
+                }
+            },
+            cellGreen: {
+                fill: {
+                    fgColor: {
+                        rgb: 'FF00FF00'
+                    }
+                }
+            }
+        };
+
+//Array of objects representing heading rows (very top)
+        const heading = [
+            listMontants.map(value => { return { value:value.description, style:styles.headerDark}})];
+
+//Here you specify the export structure
+        const specification = {
+            montant: { // <- the key should match the actual data key
+                displayName: 'montant', // <- Here you specify the column header
+                headerStyle: styles.headerDark, // <- Header style
+                cellStyle: function(value, row) { // <- style renderer function
+                    // if the status is 1 then color in green else color in red
+                    // Notice how we use another cell value to style the current one
+                    return (row.status_id == 1) ? styles.cellGreen : {fill: {fgColor: {rgb: 'FFFF0000'}}}; // <- Inline cell style is possible
+                },
+                width: 120 // <- width in pixels
+            },
+            description: { // <- the key should match the actual data key
+                displayName: 'description', // <- Here you specify the column header
+                    headerStyle: styles.headerDark, // <- Header style
+                    cellStyle: function(value, row) { // <- style renderer function
+                    // if the status is 1 then color in green else color in red
+                    // Notice how we use another cell value to style the current one
+                    return (row.status_id == 1) ? styles.cellGreen : {fill: {fgColor: {rgb: 'FFFF0000'}}}; // <- Inline cell style is possible
+                },
+                width: 120 // <- width in pixels
+            }
+        }
+
+// The data set should have the following shape (Array of Objects)
+// The order of the keys is irrelevant, it is also irrelevant if the
+// dataset contains more fields as the report is build based on the
+// specification provided above. But you should have all the fields
+// that are listed in the report specification
+        const dataset = listMontants.map(value => { return { montant:value.montant, description: value.description}})
+
+// Define an array of merges. 1-1 = A:1
+// The merges are independent of the data.
+// A merge will overwrite all data _not_ in the top-left cell.
+
+console.log(dataset)
+// Create the excel report.
+// This function will return Buffer
+        const report = excel.buildExport(
+            [ // <- Notice that this is an array. Pass multiple sheets to create multi sheet report
+                {
+                    name: 'Report', // <- Specify sheet name (optional)
+                    heading: heading,
+                    specification:specification,// <- Raw heading array (optional)// <- Merge cell ranges// <- Report specification
+                    data: dataset // <-- Report data
+                }
+            ]
+        );
+
+// You can then return this straight
+        res.attachment('report.xlsx'); // This is sails.js specific (in general you need to set headers)
+        return res.send(report);
+    }
+
 
     @Get(':id')
     async findOne(@Param('id') id): Promise<ActionDTO | void> {
@@ -29,8 +125,9 @@ export class ActionController {
         await this.actionService.delete(id);
         return 'ok'
     }
+
     @Put(':id')
-    async update(@Param('id') id, @Body() actinDTO:ActionDTO): Promise<string> {
+    async update(@Param('id') id, @Body() actinDTO: ActionDTO): Promise<string> {
         await this.actionService.update(id, actinDTO);
         return 'ok'
     }
